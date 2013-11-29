f7c61129c5b2b9abbfe1c792e7f668d989d15442
Eduardo Bart <edub4rt@gmail.com>
2013-11-29 19:21:45 -0200

    Fixes in luajit and lua build

--- LuaJIT-2.0.2/build_luajit.sh
+++ LuaJIT-2.0.2/build_luajit.sh
@@ -5,6 +5,7 @@ host_os=`uname -s | tr "[:upper:]" "[:lower:]"`
 
 SRCDIR=$DIR
 LIBDIR=$DIR/../../libs/
+NDKFLAGS="-fexceptions -ffunction-sections -funwind-tables -no-canonical-prefixes -fomit-frame-pointer -fno-strict-aliasing -finline-limit=64 -Wa,--noexecstack -DNDEBUG -DANDROID"
 
 cd "$SRCDIR"
 
@@ -13,31 +14,33 @@ NDKABI=9
 NDKVER=$NDK/toolchains/arm-linux-androideabi-4.8
 NDKP=$NDKVER/prebuilt/${host_os}-x86_64/bin/arm-linux-androideabi-
 NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-arm"
+NDKARCH="$NDKFLAGS -march=armv5te -mtune=xscale -msoft-float -mthumb"
 
 # Android/ARM, armeabi (ARMv5TE soft-float), Android 2.2+ (Froyo)
 DESTDIR=$LIBDIR/armeabi
 mkdir -p $DESTDIR
 rm "$DESTDIR"/luajit.so
 make clean
-make amalg HOST_CC="gcc -m32" CROSS=$NDKP TARGET_SYS=Linux TARGET_FLAGS="$NDKF"
+make amalg HOST_CC="gcc -m32" CROSS=$NDKP TARGET_SYS=Linux TARGET_FLAGS="$NDKF $NDKARCH"
 
-if [ -f $SRCDIR/src/libluajit.a ]; then
+if [ -f $SRCDIR/src/libluajit.so ]; then
     mv $SRCDIR/src/libluajit.so $DESTDIR/libluajit.so
 fi;
 
 # Android/ARM, armeabi-v7a (ARMv7 VFP), Android 4.0+ (ICS)
-NDKARCH="-march=armv7-a -mhard-float -Wl,--fix-cortex-a8"
+NDKARCH="$NDKFLAGS -march=armv7-a -msoft-float -mfpu=vfpv3-d16 -mthumb -Wl,--fix-cortex-a8"
 DESTDIR=$LIBDIR/armeabi-v7a
 mkdir -p $DESTDIR
 rm "$DESTDIR"/luajit.so
 make clean
 make amalg HOST_CC="gcc -m32" CROSS=$NDKP TARGET_SYS=Linux TARGET_FLAGS="$NDKF $NDKARCH"
 
-if [ -f $SRCDIR/src/libluajit.a ]; then
+if [ -f $SRCDIR/src/libluajit.so ]; then
     mv $SRCDIR/src/libluajit.so $DESTDIR/libluajit.so
 fi;
 
 # Android/x86, x86 (i686 SSE3), Android 4.0+ (ICS)
+NDKARCH="$NDKFLAGS -march=i686 -finline-limit=300"
 NDKABI=14
 DESTDIR=$LIBDIR/x86
 NDKVER=$NDK/toolchains/x86-4.8
@@ -46,9 +49,9 @@ NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-x86"
 mkdir -p $DESTDIR
 rm "$DESTDIR"/luajit.so
 make clean
-make amalg HOST_CC="gcc -m32" CROSS=$NDKP TARGET_SYS=Linux TARGET_FLAGS="$NDKF"
+make amalg HOST_CC="gcc -m32" CROSS=$NDKP TARGET_SYS=Linux TARGET_FLAGS="$NDKF $NDKARCH"
 
-if [ -f $SRCDIR/src/libluajit.a ]; then
+if [ -f $SRCDIR/src/libluajit.so ]; then
     mv $SRCDIR/src/libluajit.so $DESTDIR/libluajit.so
 fi;
 
--- lua-5.1.5/Android.mk
+++ lua-5.1.5/Android.mk
@@ -1,10 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE 		:= lua
-LOCAL_C_INCLUDES 	:= $(LOCAL_PATH)/include
-LOCAL_CFLAGS += -O2 -DLUA_USE_POSIX
+LOCAL_MODULE := lua
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_CFLAGS += -O2 -DLUA_USE_POSIX
 LOCAL_SRC_FILES := $(subst $(LOCAL_PATH)/,, $(wildcard $(LOCAL_PATH)/src/*.c))
 
 include $(BUILD_SHARED_LIBRARY)
