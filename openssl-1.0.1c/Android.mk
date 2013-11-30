f54fff809e52a656284270193f9801fa7336183f
Eduardo Bart <edub4rt@gmail.com>
2013-11-30 13:31:15 -0200

    Fixes

--- openssl-1.0.1c/Android.mk
+++ openssl-1.0.1c/Android.mk
@@ -3,7 +3,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := openssl
 
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 subdirs := $(addprefix $(LOCAL_PATH)/,$(addsuffix /Android.mk, \
 		crypto \
 		ssl \
